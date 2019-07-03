eff7fce7707e4d48c5dc5cf69da0dbf3f2405743
Matias Spinelli <matias.spinelli@wolox.com.ar>
2019-07-03 11:48:02 -0300

    alamofire

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'MSFrameworkCocoapods' do
+
+  use_frameworks!
+  pod 'Alamofire'
+
+end
---
+++ Pods/Target Support Files/Pods-MSFrameworkCocoapods/Pods-MSFrameworkCocoapods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MSFrameworkCocoapods : NSObject
+@end
+@implementation PodsDummy_Pods_MSFrameworkCocoapods
+@end
