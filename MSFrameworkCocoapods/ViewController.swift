7b3ebf98deea770e46a5177e7353662291f5ac64
Matias Spinelli <matias.spinelli@wolox.com.ar>
2019-07-03 11:44:59 -0300

    Initial Commit

---
+++ MSFrameworkCocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSFrameworkCocoapods
+//
+//  Created by Matias Spinelli on 03/07/2019.
+//  Copyright © 2019 Matias Spinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
